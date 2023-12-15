d18d366a9b3b037d4fe7bb7dfe86662dda1a1cb8
Desmond Nzubechukwu Abugu <nzubestdesmond@gmail.com>
2023-12-15 22:02:51 +0100

    added maths operator

---
+++ MATHS OPERATORS/index.js
@@ -0,0 +1,38 @@
+// Basic Operators
+
+// Arithmetic Operators
+let sum = 5 + 3; // Result: 8
+let difference = 10 - 4; // Result: 6
+let product = 7 * 2; // Result: 14
+let quotient = 20 / 4; // Result: 5
+let remainder = 15 % 4; // Result: 3
+
+// Assignment Operators
+let x = 10; // Assigns the value 10 to variable x
+let y = 5;
+y += 3; // Equivalent to y = y + 3; // Result: 8
+
+// Comparison Operators
+let isEqual = (5 == '5'); // Result: true (loose equality)
+let isStrictEqual = (5 === '5'); // Result: false (strict equality)
+let notEqual = (10 != '10'); // Result: false
+
+// Logical Operators
+let andResult = (true && false); // Result: false
+let orResult = (true || false); // Result: true
+let notResult = !true; // Result: false
+
+// Math in JavaScript
+
+// Math Functions
+let roundedNum = Math.round(4.7); // Result: 5
+let ceiledNum = Math.ceil(4.2); // Result: 5
+let flooredNum = Math.floor(4.9); // Result: 4
+let randomNum = Math.random(); // Result: (e.g., 0.723891)
+let maxNum = Math.max(2, 7, 1, 9); // Result: 9
+let minNum = Math.min(2, 7, 1, 9); // Result: 1
+
+// Exponential and Logarithmic Functions
+let powerResult = Math.pow(2, 3); // Result: 8
+let sqrtResult = Math.sqrt(25); // Result: 5
+let logResult = Math.log(10); // Result: 2.302585092994046

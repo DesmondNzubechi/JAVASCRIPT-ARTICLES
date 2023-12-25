f917c31e9406fdf216336d473636ce0365b1bf37
Desmond Nzubechukwu Abugu <nzubestdesmond@gmail.com>
2023-12-25 09:33:07 +0100

    added datatype

---
+++ DATA TYPE/index.js
@@ -0,0 +1,30 @@
+//PRIMITIVE DATA TYPES
+
+// 1. Undefined
+let x;
+console.log(x); // Output: undefined
+
+// 2. Null
+let y = null;
+console.log(y); // Output: null
+
+// 3. Boolean
+let isTrue = true;
+let isFalse = false;
+
+// 4. Number
+let integerNumber = 42;
+let floatNumber = 3.14;
+
+// 5. String
+let greeting = 'Hello, World!';
+let message = "JavaScript is fun!";
+
+// 6. Symbol
+const symbol1 = Symbol('symbol');
+const symbol2 = Symbol('symbol');
+console.log(symbol1 === symbol2); // Output: false
+
+// 7. BigInt
+let bigInteger = 9007199254740991n; // 'n' suffix indicates BigInt
+console.log(bigInteger); // Output: 9007199254740991n

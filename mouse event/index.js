45a5416fb722ad7228c9410a11c3213b83b051bc
Desmond Nzubechukwu Abugu <nzubestdesmond@gmail.com>
2024-03-16 09:24:02 +0100

    added pointer event

---
+++ mouse event/index.js
@@ -0,0 +1,14 @@
+
+
+
+// Example: Adding a click event listener to a button element
+const button = document.getElementById('myButton');
+
+button.addEventListener('click', function(event) {
+  // Execute custom functionality when the button is clicked
+  console.log('Button clicked!');
+});
+
+
+
+
---
+++ pointer events/index.js
@@ -0,0 +1,17 @@
+let element = document.getElementById("myForm")
+
+// Add event listener for pointerdown event
+element.addEventListener('pointerdown', function(event) {
+    // Handle pointer down event
+  });
+  
+  // Add event listener for pointermove event
+  element.addEventListener('pointermove', function(event) {
+    // Handle pointer move event
+  });
+  
+  // Add event listener for pointerup event
+  element.addEventListener('pointerup', function(event) {
+    // Handle pointer up event
+  });
+  
